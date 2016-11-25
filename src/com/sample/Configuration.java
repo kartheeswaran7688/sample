e9dac4c65c3658d7bd8f60116a66d4023bc9c9c4
kartheeswaran <kartheeswaran.v@quikr.com>
2016-11-25 15:06:38 +0530

    Added Changes to Config

--- src/com/sample/Configuration.java
+++ src/com/sample/Configuration.java
@@ -1,5 +1,14 @@
 package com.sample;
 
 public class Configuration {
-
+ private int conflict_value;
+ private String name1;
+ 
+ private String getName() {
+	 return name1;
+ }
+ 
+ private int getValue() {
+	 return conflict_value;
+ }
 }
